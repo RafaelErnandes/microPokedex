5a9a52724612181ee702f0dec56c6b60d177fb09
Rafael Ernandes <ernandesrafael908@gmail.com>
2025-03-28 13:48:06 -0300

    feat(genButton): adiciona animacao nos botoes

--- src/components/GenButton/index.tsx
+++ src/components/GenButton/index.tsx
@@ -6,8 +6,10 @@ export const GenButton = (props: GenButtonProps) => {
   return (
     <button
       onClick={() => onClick(generation)}
-      className={`px-4 py-2 rounded-md text-white font-semibold transition-all duration-300 ${
-        isSelected ? "bg-primary" : "bg-genButton"
+      className={`px-4 py-2 rounded-md text-white font-semibold transition-all duration-300 w-12 ${
+        isSelected
+          ? "bg-red-700 shadow-lg transform translate-y-1 active:translate-y-0 active:shadow-md"
+          : "bg-genButton"
       } cursor-pointer`}
     >
       {children}
